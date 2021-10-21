48230dbf740f9d192be3fd335e3ce74f6494d893
Abhinandan Shrestha <shtabhi@gmail.com>
2021-10-21 18:31:40 +0545

    done

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -1,24 +1,31 @@
 from rest_framework import serializers
 from .models import Article
 
-class ArticleSerializer(serializers.Serializer):
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ('id','title','author')
+
+# class ArticleSerializer(serializers.Serializer):
     # from models.py 
     # title = models.CharField(max_length=200)
     # author = models.CharField(max_length=200)
     # email = models.EmailField(max_length=200)
     # date = models.DateTimeField(auto_now_add=True)
 
-    title = serializers.CharField(max_length=200)
-    author = serializers.CharField(max_length=200)
-    email = serializers.CharField(max_length=200)
-    date = serializers.DateTimeField()
+    # title = serializers.CharField(max_length=200)
+    # author = serializers.CharField(max_length=200)
+    # email = serializers.CharField(max_length=200)
+    # date = serializers.DateTimeField()
 
-    def create(self,validated_data):
-        return Article.objects.create(validated_data)
+    # def create(self,validated_data):
+    #     return Article.objects.create(validated_data)
 
-    def update(self,instance, validated_data):
-        instance.title = validated_data.get('title',instance.title)
-        instance.author = validated_data.get('author',instance.author)
-        instance.email = validated_data.get('email',instance.email)
-        instance.date = validated_data.get('date',instance.date)
-        instance.save()
+    # def update(self,instance, validated_data):
+    #     instance.title = validated_data.get('title',instance.title)
+    #     instance.author = validated_data.get('author',instance.author)
+    #     instance.email = validated_data.get('email',instance.email)
+    #     instance.date = validated_data.get('date',instance.date)
+    #     instance.save()
+
+
---
+++ api_basic/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('article',views.article_list)
+]
